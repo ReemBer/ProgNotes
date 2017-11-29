74fae634922a41fee5f94a20cb9aa1dcd94325e3
v.tarasevich <tarasevich.vlad.97@gmail.com>
2017-11-29 19:31:08 +0300

    format minimal configurations

---
+++ src/main/java/by/bsuir/prognotes/PrognotesApplication.java
@@ -0,0 +1,12 @@
+package by.bsuir.prognotes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrognotesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrognotesApplication.class, args);
+	}
+}
